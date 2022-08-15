3d918413c679fa86d5fcefe58202ef5d7f92dc3e
ramenha0141 <ramenhaoishii@gmail.com>
2022-08-15 18:36:43 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -60,9 +60,7 @@ function withDebug(config: UserConfig): UserConfig {
         config.plugins = (config.plugins || []).concat({
             name: 'electron-vite-debug',
             configResolved(config) {
-                const index = config.plugins.findIndex(
-                    (p) => p.name === 'electron-main-watcher'
-                );
+                const index = config.plugins.findIndex((p) => p.name === 'electron-main-watcher');
                 // At present, Vite can only modify plugins in configResolved hook.
                 (config.plugins as Plugin[]).splice(index, 1);
             }
@@ -113,12 +111,7 @@ function renderBuiltUrl(): Plugin {
         config(config) {
             config.experimental = {
                 renderBuiltUrl(filename, type) {
-                    if (
-                        KNOWN_ASSET_TYPES.includes(
-                            path.extname(filename).slice(1)
-                        ) &&
-                        type.hostType === 'js'
-                    ) {
+                    if (KNOWN_ASSET_TYPES.includes(path.extname(filename).slice(1)) && type.hostType === 'js') {
                         // Avoid Vite relative-path assets handling
                         // https://github.com/vitejs/vite/blob/89dd31cfe228caee358f4032b31fdf943599c842/packages/vite/src/node/build.ts#L838-L875
                         return { runtime: JSON.stringify(filename) };
